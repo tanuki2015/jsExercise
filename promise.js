59b8f0dfee537d79684706d009c46e0f52f2ff0b
tanuki2015 <75954762@qq.com>
2017-02-23 17:48:48 +0800

    promise

---
+++ promise.js
@@ -0,0 +1,26 @@
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('等待2秒');
+    },2000);
+});
+
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('等待3秒');
+},3000);
+    });
+
+// 单纯的promise用法
+// promise2
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error));
+//
+// promise3
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error));
+
+// promise.all的用法
+const promiseAll = Promise.all([promise2, promise3]);
+promiseAll
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
